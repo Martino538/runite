06e1c38003f18050342e6dcb916c0dedef5b6589
Martijn Kooijman <32341318+Martino538@users.noreply.github.com>
2025-10-15 01:58:44 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector('.hamburger');
   const mobileNav = document.querySelector('nav');
   const header = document.querySelector('header');
+  const topVotesMenu = document.querySelector('#topVotesMenu');
+  const spotlight = document.querySelector('#spotlightMenu');
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileNav.classList.toggle('active');
     header.classList.toggle('active');
+  });
+
+  topVotesMenu.addEventListener('click', () => {
+    topVotesMenu.classList.toggle('hide');
+  });
+
+  spotlight.addEventListener('click', () => {
+    spotlight.classList.toggle('hide');
+  });
+
+
+  const topVotesInner = document.querySelector('#topVotesMenu .inner-content');
+  const closeIcon = document.querySelector('#topVotesMenu .close-tag i');
+
+  topVotesInner.addEventListener('click', () => {
+    if (closeIcon.classList.contains('fa-minus')) {
+      closeIcon.classList.replace('fa-minus', 'fa-plus');
+    } else {
+      closeIcon.classList.replace('fa-plus', 'fa-minus');
+    }
+  });
+
+  const spotlightInner = document.querySelector('#spotlight .inner-content');
+  const closeIcon2 = document.querySelector('#spotlight .close-tag i');
+
+  spotlightInner.addEventListener('click', () => {
+    if (closeIcon.classList.contains('fa-minus')) {
+      closeIcon2.classList.replace('fa-minus', 'fa-plus');
+    } else {
+      closeIcon2.classList.replace('fa-plus', 'fa-minus');
+    }
   });
 
   const swiper = new Swiper(".mySwiper", {
